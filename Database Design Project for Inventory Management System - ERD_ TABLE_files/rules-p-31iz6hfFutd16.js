3922b490c615898f9572bf29c84d81ffb3011407
Mahim C <mahimc@bu.edu>
2022-09-09 09:36:44 -0400

    testing all docs

---
+++ Database Design Project for Inventory Management System - ERD_ TABLE_files/rules-p-31iz6hfFutd16.js
@@ -0,0 +1,5 @@
+/*
+ Quantcast measurement tag
+ Copyright (c) 2008-2022, Quantcast Corp.
+*/
+'use strict';(function(a,b,c){window.__qc("fire","https://pxl.qccerttest.com");__qc("rules",[a])})("p-31iz6hfFutd16",window,document);
